dc5388d308d20ae08ebc649b07aa8865b9dd2ff7
Max Hart <maxhart5000@icloud.com>
2024-02-21 23:55:19 +0900

    Add Bootstrap CSS and import new components. Create a navbar in the header component.

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrl: './recipe-list.component.css'
+})
+export class RecipeListComponent {
+}
