f16a428e4484aaade89490f4ca77c4fd424e5a68
Max Hart <maxhart5000@icloud.com>
2024-02-22 12:18:16 +0900

    Update dependencies and improve UI layout bootstrap and custom css stlying. Create a template layout to build upon.

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,9 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrl: './recipe-list.component.css'
 })
-export class RecipeListComponent {
+export class RecipeListComponent implements OnInit{
+  recipes: Recipe[] = [
+    new Recipe("Lasagna","Grandma's home cooked Lasagna", "https://images.pixexid.com/delicious-lasagna-y3fwjqie.jpeg"),
+    new Recipe("Chicken Alfredo","Creamy Chicken Alfredo", "https://upload.wikimedia.org/wikipedia/commons/4/4c/Feast_of_the_Seven_Fishes_5.jpg"),
+    new Recipe("Spaghetti","Classic Spaghetti", "https://www.inspiredtaste.net/wp-content/uploads/2019/03/Spaghetti-with-Meat-Sauce-Recipe-1-1200.jpg")
+  ];
+  
+  constructor() {
+
+  }
+  ngOnInit(): void {
+    
+  }
 }
